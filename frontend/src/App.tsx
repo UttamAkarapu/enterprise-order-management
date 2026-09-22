178372d1d0f652b1c9c3772b5eb0e565c73d3193
Uttam Akarapu <uttamadeveloper@gmail.com>
2026-09-22 19:29:42 +0530

    Initial project setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1>Enterprise Order Management Portal</h1>
+
+      <p>
+        React + TypeScript + Redux Toolkit
+      </p>
+    </div>
+  );
+}
+
+export default App;
